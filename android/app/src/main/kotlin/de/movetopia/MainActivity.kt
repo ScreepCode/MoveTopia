7b640fe8144ae348893c61c918addbf2d6862250
Niklas Buse <nicki_bu@gmx.de>
2024-09-30 15:59:21 +0200

    fix(android): Use FlutterFragmentActivity
    This commit changes `MainActivity` to extend `FlutterFragmentActivity` instead of `FlutterActivity`.
    This error where introduced while renaming (and generating) the new app name

--- android/app/src/main/kotlin/de/movetopia/MainActivity.kt
+++ android/app/src/main/kotlin/de/movetopia/MainActivity.kt
@@ -1,5 +1,5 @@
 package de.movetopia
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
