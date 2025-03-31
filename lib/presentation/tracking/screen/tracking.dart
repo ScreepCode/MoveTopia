f878e403ac43452f4bd630df98c34ad9762098f5
Jozys <joshua@slaar.de>
2025-03-31 17:39:32 +0200

    ♻️ Enable back navigation in activity tracking

--- lib/presentation/tracking/screen/tracking.dart
+++ lib/presentation/tracking/screen/tracking.dart
@@ -8,7 +8,6 @@ import 'package:movetopia/presentation/tracking/view_model/tracking_view_model.d
 import 'package:movetopia/presentation/tracking/widgets/current_activity.dart';
 import 'package:movetopia/presentation/tracking/widgets/start_activity.dart';
 import 'package:movetopia/utils/health_utils.dart';
-import 'package:movetopia/utils/tracking_utils.dart';
 
 class TrackingScreen extends HookConsumerWidget {
   const TrackingScreen({super.key});
@@ -16,101 +15,40 @@ class TrackingScreen extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final trackingState = ref.watch(trackingViewModelProvider);
-    void startTimer() {
-      Future.delayed(const Duration(seconds: 1), () {
-        ref.read(trackingViewModelProvider.notifier).updateDuration(getDuration(
-            trackingState?.activity?.startDateTime ?? 0,
-            DateTime.now().millisecondsSinceEpoch));
-        if (trackingState?.isRecording == true) {
-          startTimer();
-        } else {
-          return;
-        }
-      });
-    }
-
-    void _showBackDialog() {
-      showDialog<void>(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text(AppLocalizations.of(context)!.tracking_leave_title),
-            content: Text(AppLocalizations.of(context)!.tracking_leave_message),
-            actions: <Widget>[
-              TextButton(
-                style: TextButton.styleFrom(
-                  textStyle: Theme.of(context).textTheme.labelLarge,
-                ),
-                child:
-                    Text(AppLocalizations.of(context)!.tracking_leave_cancel),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  textStyle: Theme.of(context).textTheme.labelLarge,
-                ),
-                child:
-                    Text(AppLocalizations.of(context)!.tracking_leave_confirm),
-                onPressed: () {
-                  Navigator.pop(context);
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          );
-        },
-      );
-    }
 
     useEffect(() {
       if (trackingState?.isRecording == true) {
-        startTimer();
+        ref.read(trackingViewModelProvider.notifier).startTimer();
       }
       return null;
     }, [trackingState?.isRecording]);
 
-    bool canPop() {
-      return trackingState == null || trackingState.isRecording == false;
-    }
-
-    return PopScope(
-        canPop: canPop(),
-        onPopInvoked: (bool didPop) {
-          if (didPop) {
-            return;
-          } else {
-            _showBackDialog();
-          }
-        },
-        child: Scaffold(
-            appBar: AppBar(
-              title: trackingState!.isRecording
-                  ? Text(getTranslatedActivityType(
-                      context,
-                      HealthWorkoutActivityType.values.firstWhere((t) =>
-                          t.name == trackingState.activity.activityType?.name)))
-                  : Text(AppLocalizations.of(context)!.tracking_title),
-            ),
-            body: trackingState.activity.activityType == ActivityType.unknown
-                ? StartActivity(
-                    onStart: (activityType) {
-                      ref
-                          .read(trackingViewModelProvider.notifier)
-                          .startTracking(activityType);
-                    },
-                  )
-                : CurrentActivity(
-                    activity: trackingState.activity,
-                    onStop: () {
-                      ref
-                          .read(trackingViewModelProvider.notifier)
-                          .stopTracking();
-                    },
-                    startTimer: startTimer,
-                    duration: trackingState.duration,
-                    isRecording: trackingState!.isRecording,
-                  )));
+    return Scaffold(
+        appBar: AppBar(
+          title: trackingState!.isRecording
+              ? Text(getTranslatedActivityType(
+                  context,
+                  HealthWorkoutActivityType.values.firstWhere((t) =>
+                      t.name == trackingState.activity.activityType?.name)))
+              : Text(AppLocalizations.of(context)!.tracking_title),
+        ),
+        body: trackingState.activity.activityType == ActivityType.unknown
+            ? StartActivity(
+                onStart: (activityType) {
+                  ref
+                      .read(trackingViewModelProvider.notifier)
+                      .startTracking(activityType);
+                },
+              )
+            : CurrentActivity(
+                activity: trackingState.activity,
+                onStop: () {
+                  ref.read(trackingViewModelProvider.notifier).stopTracking();
+                },
+                startTimer:
+                    ref.read(trackingViewModelProvider.notifier).startTimer,
+                duration: trackingState.duration,
+                isRecording: trackingState!.isRecording,
+              ));
   }
 }
