2f4c145fded188826902423c1a12fb39c424abb5
Jozys <joshua@slaar.de>
2025-04-05 11:32:06 +0200

    💄Show activity details only if values are available
    - Prepare displaying step count
    - Add localized duration display

--- lib/presentation/activities/details/widget/workout_details.dart
+++ lib/presentation/activities/details/widget/workout_details.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:movetopia/presentation/common/widgets/generic_card.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:movetopia/presentation/common/widgets/generic_card.dart';
 
 import 'detail_stats_card.dart';
 
@@ -8,13 +8,14 @@ class WorkoutDetails extends StatelessWidget {
   final int averageHeartBeat;
   final int duration;
   final int caloriesBurnt;
-
+  final int steps;
 
   const WorkoutDetails({
     super.key,
     required this.averageHeartBeat,
     required this.duration,
     required this.caloriesBurnt,
+    required this.steps,
   });
 
   @override
@@ -28,18 +29,25 @@ class WorkoutDetails extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           DetailStatsCardEntry(
-            displayName: l10n.activity_duration,
-            value:
-                "${(duration / 60).toStringAsFixed(0)} min ${((duration / 60 % 1) * 60).toStringAsFixed(0)} sec",
-          ),
-          DetailStatsCardEntry(
-            displayName: l10n.activity_avg_heart_rate,
-            value: '$averageHeartBeat bpm',
-          ),
-          DetailStatsCardEntry(
-            displayName: l10n.activity_burnt_calories,
-            value: '$caloriesBurnt kcal',
-          )
+              displayName: l10n.activity_duration,
+              value: AppLocalizations.of(context)!.activity_duration_text(
+                  (duration / 60).toStringAsFixed(0),
+                  ((duration / 60 % 1) * 60).toStringAsFixed(0))),
+          if (averageHeartBeat != 0)
+            DetailStatsCardEntry(
+              displayName: l10n.activity_avg_heart_rate,
+              value: '$averageHeartBeat bpm',
+            ),
+          if (steps != 0)
+            DetailStatsCardEntry(
+              displayName: l10n.activity_steps,
+              value: '$steps',
+            ),
+          if (caloriesBurnt != 0)
+            DetailStatsCardEntry(
+              displayName: l10n.activity_burnt_calories,
+              value: '$caloriesBurnt kcal',
+            )
         ],
       ),
     );
