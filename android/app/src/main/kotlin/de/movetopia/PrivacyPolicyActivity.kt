c8d3e080b97d144fe38baea6b5b01d70232c3459
Niklas Buse <63686539+ScreepCode@users.noreply.github.com>
2025-04-05 23:13:28 +0200

    ✨ Add Privacy Policy activity and HTML file, update manifest and buil… (#78)

---
+++ android/app/src/main/kotlin/de/movetopia/PrivacyPolicyActivity.kt
@@ -0,0 +1,29 @@
+package de.movetopia
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import de.movetopia.BuildConfig
+
+/**
+ * Activity to show privacy policy.
+ * This activity is used as a target for the Health Connect app's
+ * permission usage screen and automatically opens the privacy policy in the browser.
+ */
+class PrivacyPolicyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Nutze die URL aus den BuildConfig-Feldern
+        val privacyPolicyUrl = BuildConfig.PRIVACY_POLICY_URL
+
+        // Open privacy policy in browser
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(privacyPolicyUrl))
+        startActivity(intent)
+
+        // Finish this activity after opening the browser
+        finish()
+    }
+} 
