1842ca12521ec83e6bbb759614ac152b474342eb
Jozys <joshua@slaar.de>
2025-05-05 09:08:10 +0200

    ♻️ Clean up getInstalledAppIcon function by removing unused logging

--- lib/utils/system_utils.dart
+++ lib/utils/system_utils.dart
@@ -1,22 +1,15 @@
 import 'dart:typed_data';
 
-import 'package:logging/logging.dart';
-
 Future<Uint8List?> getInstalledAppIcon(String sourceId) async {
-  final log = Logger("InstalledAppIcon");
   if (sourceId.isNotEmpty) {
-    try {
-      // Sadly due to the QUERY_ALL_PACKAGES permission not being allowed for release in the Play Store in this use case, this
-      // method is not usable in production. Therefore we have to disable it for now.
-      // TODO: Find a way to get the app icon without using QUERY_ALL_PACKAGES
-      // var appIcon =
-      //     (await InstalledApps.getAppInfo(sourceId.toString(), null))?.icon;
-      // if (appIcon != null && appIcon.isNotEmpty) {
-      //   return appIcon;
-      // }
-    } catch (_) {
-      log.info("App icon fetching failed");
-    }
+    // Sadly due to the QUERY_ALL_PACKAGES permission not being allowed for release in the Play Store in this use case, this
+    // method is not usable in production. Therefore we have to disable it for now.
+    // TODO: Find a way to get the app icon without using QUERY_ALL_PACKAGES
+    // var appIcon =
+    //     (await InstalledApps.getAppInfo(sourceId.toString(), null))?.icon;
+    // if (appIcon != null && appIcon.isNotEmpty) {
+    //   return appIcon;
+    // }
   }
   return null;
 }
