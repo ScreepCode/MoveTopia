3c32261b04a8199b590f2246cce53be96f16c1be
Jozys <joshua@slaar.de>
2025-05-05 09:02:19 +0200

    ♻️ Remove installed_apps dependency and update related code comments
    - This is due to QUERY_ALL_PACKAGES not being allowed to be used in production for our use case

--- lib/utils/system_utils.dart
+++ lib/utils/system_utils.dart
@@ -1,17 +1,19 @@
 import 'dart:typed_data';
 
-import 'package:installed_apps/installed_apps.dart';
 import 'package:logging/logging.dart';
 
 Future<Uint8List?> getInstalledAppIcon(String sourceId) async {
   final log = Logger("InstalledAppIcon");
   if (sourceId.isNotEmpty) {
     try {
-      var appIcon =
-          (await InstalledApps.getAppInfo(sourceId.toString(), null))?.icon;
-      if (appIcon != null && appIcon.isNotEmpty) {
-        return appIcon;
-      }
+      // Sadly due to the QUERY_ALL_PACKAGES permission not being allowed for release in the Play Store in this use case, this
+      // method is not usable in production. Therefore we have to disable it for now.
+      // TODO: Find a way to get the app icon without using QUERY_ALL_PACKAGES
+      // var appIcon =
+      //     (await InstalledApps.getAppInfo(sourceId.toString(), null))?.icon;
+      // if (appIcon != null && appIcon.isNotEmpty) {
+      //   return appIcon;
+      // }
     } catch (_) {
       log.info("App icon fetching failed");
     }
